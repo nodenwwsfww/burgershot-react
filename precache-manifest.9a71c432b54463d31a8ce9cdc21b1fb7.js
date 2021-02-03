9061447aaa96885481cdf93dfb16e783210201ec
Roman Radchenko <nodenwwsfww@gmail.com>
2021-02-03 17:01:02 +0300

    Updates

---
+++ precache-manifest.9a71c432b54463d31a8ce9cdc21b1fb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b4cae9131eea61ac796414bfa13f5f",
+    "url": "/burgershot-MERN/index.html"
+  },
+  {
+    "revision": "20f71f8d67d26f55299e",
+    "url": "/burgershot-MERN/static/js/2.90d5f3a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgershot-MERN/static/js/2.90d5f3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4d045aad1e768bf90a",
+    "url": "/burgershot-MERN/static/js/main.5299853e.chunk.js"
+  },
+  {
+    "revision": "ccf6967fa5c335840e7c",
+    "url": "/burgershot-MERN/static/js/runtime-main.e32d21a6.js"
+  },
+  {
+    "revision": "d202f292dbea01ed6675e21169f90dd9",
+    "url": "/burgershot-MERN/static/media/banner.d202f292.svg"
+  },
+  {
+    "revision": "cfe37dbc7d784a341d33f03dba29f608",
+    "url": "/burgershot-MERN/static/media/logo.cfe37dbc.svg"
+  },
+  {
+    "revision": "6ea25215d505d786c94a8f392b4eef5f",
+    "url": "/burgershot-MERN/static/media/sign.6ea25215.svg"
+  },
+  {
+    "revision": "db2d7696bf5fbf9ea7d203ce6639a44f",
+    "url": "/burgershot-MERN/static/media/trash.db2d7696.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.796da58151b861e604e5578f0222986c.js"
+  "/burgershot-MERN/precache-manifest.9a71c432b54463d31a8ce9cdc21b1fb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burgershot-MERN/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
