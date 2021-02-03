1e28856a3463b05a718de78946f1fbee5e26d722
Roman Radchenko <nodenwwsfww@gmail.com>
2021-02-03 15:44:46 +0300

    save build for githubpages

---
+++ build/precache-manifest.eced50e4e8cc471255e8df3d67007259.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776cfb99afc3db706d297dccefaa958b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43eb8a3fbf7d4b7bfc57",
+    "url": "/static/js/2.2667648d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2667648d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2808b05348ceaa1cbd1f",
+    "url": "/static/js/main.29fdcb51.chunk.js"
+  },
+  {
+    "revision": "2364851f5de5ed61a33b",
+    "url": "/static/js/runtime-main.38d66a2b.js"
+  },
+  {
+    "revision": "d202f292dbea01ed6675e21169f90dd9",
+    "url": "/static/media/banner.d202f292.svg"
+  },
+  {
+    "revision": "cfe37dbc7d784a341d33f03dba29f608",
+    "url": "/static/media/logo.cfe37dbc.svg"
+  },
+  {
+    "revision": "6ea25215d505d786c94a8f392b4eef5f",
+    "url": "/static/media/sign.6ea25215.svg"
+  },
+  {
+    "revision": "db2d7696bf5fbf9ea7d203ce6639a44f",
+    "url": "/static/media/trash.db2d7696.svg"
+  }
+]);
