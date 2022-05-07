8009b76db9f9af98fc6454d6a296f41746e791d0
Roman Radchenko <nodenwwsfww@gmail.com>
2022-05-07 18:38:02 +0300

    style: add images in img folder

--- frontend/src/Components/Menu/Banner.tsx
+++ frontend/src/Components/Menu/Banner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import banner from '../../img/banner.jpeg';
+import banner from '../../img/banner.svg';
 
 const BannerStyled = styled.div`
   background: url(${banner}) no-repeat center/cover;
