c4650fb4324ddac3c3355f486bd7409cd6f5df4a
Roman Radchenko <nodenwwsfww@gmail.com>
2022-05-07 17:25:00 +0300

    fix:add type for useOpenItem in hooks

--- frontend/src/Hooks/useOpenItem.ts
+++ frontend/src/Hooks/useOpenItem.ts
@@ -1,6 +1,7 @@
 import {useState} from 'react';
+import {IMenuItem} from '../model';
 
 export default function useOpenItem() {
-    const [openItem, setOpenItem] = useState(null);
+    const [openItem, setOpenItem] = useState<IMenuItem | null>(null);
     return {openItem, setOpenItem};
 }
